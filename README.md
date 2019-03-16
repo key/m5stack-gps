de4483902c8d8ca3388731307d01118a79ed9c97
Mitsukuni Sato <mitsukuni.sato@gmail.com>
2019-03-17 01:24:32 +0900

    added g-bowl sample

--- README.md
+++ README.md
@@ -9,6 +9,9 @@ M5Stack と GPS Module (u-blox NEO-M8N)を使ったコード片を集めてい�
 
 ## ディレクトリの説明
 
+- mpu9250_gbowl
+  - G ボウルを描画します
+  - MPU9250 搭載モデル専用
 - satellite_view
   - 衛星描画します
   - [Ambient さんのブログ](https://ambidata.io/blog/2018/10/12/m5stack_gpswatch/)にあったコードを pyflake にかけて清書しました
---
+++ mpu9250_gbowl/main.py
@@ -0,0 +1,33 @@
+# tested on M5Stack Gray
+# based on http://blog.akanumahiroaki.com/entry/2018/10/27/235000
+from m5stack import lcd
+from machine import I2C
+from mpu9250 import MPU9250
+import time
+
+i2c = I2C(sda = 21, scl = 22)
+sensor = MPU9250(i2c)
+
+lcd.clear()
+
+x, y = 0, 0
+
+while True:
+    ax, ay, az = sensor.acceleration
+    ax = ax * -1  # original is left negative
+
+    # clear previous ball
+    lcd.circle(x, y, 5, lcd.BLACK, lcd.BLACK)
+
+    # g-bowl
+    lcd.circle(160, 120, 110, lcd.WHITE)
+    lcd.circle(160, 120, 55, lcd.WHITE)
+    lcd.line(50, 120, 270, 120, lcd.WHITE)
+    lcd.line(160, 10, 160, 230, lcd.WHITE)
+
+    # plot ball
+    x = int(ax * (55 / 10)) + 160
+    y = int(ay * (55 / 10)) + 120
+    lcd.circle(x, y, 5, lcd.RED, lcd.RED)
+
+    time.sleep_ms(20)
